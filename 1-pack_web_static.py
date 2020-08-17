be0fdf5c9206ea629ef46382b622f9b2c3f04252
jchois <choisjuliana@gmail.com>
2020-08-17 18:45:00 -0500

    Compress before sending

---
+++ 1-pack_web_static.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Compress before sending"""
+
+from fabric.api import *
+from datetime import datetime
+
+
+def do_pack():
+    """do pack module
+    """
+
+    file_name = "web_static_" + \
+        datetime.now().strftime("%Y%m%d%H%M%S") + ".tgz"
+    save_dir = "versions/"
+
+    local("mkdir -p " + save_dir)
+    chk = local("tar -cvzf {}{} web_static".format(save_dir, file_name))
+    if chk.succeeded:
+        return (save_dir + file_name)
+    else:
+        return (None)
