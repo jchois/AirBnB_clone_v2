5bf4a49bb67ffcefe6c5d1f2be1e35987e4f6e81
jchois <choisjuliana@gmail.com>
2020-09-02 02:33:00 -0500

    Fixing pep8

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -10,10 +10,12 @@ def hello():
     """display Hello HBNB!"""
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def show_hbnb():
     """display HBNB"""
     return 'HBNB'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
