6bbd38a1ebdeec93b02ac7d2306fc7f982e2aa67
jchois <choisjuliana@gmail.com>
2020-09-02 15:51:27 -0500

    Task 8 fixed

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,20 +3,19 @@
 
 from flask import Flask, render_template
 from models import storage
-from models.state import State
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """display a HTML page with a sorted list listed"""
-    states = storage.all(State)
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
     return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown_db(self):
-    """close"""
+def teardown_db(exception):
+    """closes storage"""
     storage.close()
 
 
